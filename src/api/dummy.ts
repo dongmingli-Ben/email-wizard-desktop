b67b7db91ddbc3bb96ffe5a3cc8bcc69e987da78
nbe <dongmingli_Ben@126.com>
2023-11-22 10:51:37 -0800

    add dummy events for UI

---
+++ src/api/dummy.ts
@@ -0,0 +1,153 @@
+export const DUMMY_MAILBOXES = [
+  {
+    username: "dummy@example.com",
+    protocol: "gmail",
+  },
+];
+
+export const DUMMY_EVENTS = [
+  {
+    event_type: "registration",
+    end_time: "2023-11-06T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-06T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-06T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-06T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-06T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-06T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-06T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-16T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-16T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-16T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-06T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-16T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-22T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-22T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-22T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-22T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-22T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-26T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-26T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-26T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-26T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-26T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-26T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+  {
+    event_type: "registration",
+    end_time: "2023-11-26T12:00:00 Asia/Shanghai",
+    summary: "2023大学......",
+    venue: "https://....../vm/YVgulbu.aspx",
+  },
+];
